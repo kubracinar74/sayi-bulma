8a275773a0c51380f73059d9d713a71bdc1a319d
kubracinar74 <kubracinar74@gmail.com>
2024-10-31 00:07:04 +0300

    Proje başlangıç dosyaları

---
+++ Sayi_Bulma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sayi_Bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 15;
+             
+            Console.WriteLine("Lütfen aklınızdan geçen bir sayiyi yazınız");
+
+            Console.WriteLine("Tebrikler buldunuz."); 
+        }
+    }
+}
